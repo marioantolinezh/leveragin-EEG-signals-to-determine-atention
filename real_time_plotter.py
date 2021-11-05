c8062854a3b3b741349b4a85af7b111f4169b4c5
marioantolienzh <50441834+marioantolienzh@users.noreply.github.com>
2021-11-04 21:22:45 -0400

    version4
    needs pyIDE tests (spicy lib error)
    needs data rate adjustments to plot generation

--- real_time_plotter.py
+++ real_time_plotter.py
@@ -1,38 +1,55 @@
 import serial
-
-# Import matplotlib and set the 'Qt4Agg' backend to support interactive mode on Windows and macOS
-import matplotlib
-#matplotlib.use('Qt4Agg')
-
-# Activate interactive mode
 import matplotlib.pyplot as plt
-#plt.ion()
-
+from scipy import signal
 import numpy as np
+
+
+plt.close('all')
+fig=plt.figure(1)
 plt.ion()
-fig=plt.figure()
 
 
 i=0
 x=list()
 y=list()
-i=0
-ser = serial.Serial('/dev/cu.usbmodem14201',9600)
+
+ser = serial.Serial('/dev/cu.usbmodem14101',9600)
 ser.close()
 ser.open()
 
+sampling_freq = 2000
+sample_duration = 0.1
+t = np.arange(0.0, sample_duration, 1/sampling_freq)
+
+filter = signal.firwin(400, [0.01, 0.06], pass_zero=False)
+
+
 while True:
-
-    data = ser.readline()
-    print(data.decode())
-    x.append(i)
-    y.append(data.decode())
-
-    plt.scatter(i, float(data.decode()))
-    i += 1
-    plt.show()
-    plt.pause(0.0001)  # Note this correction
-
+        data = ser.readline()
+        decoded_data = data.decode()
+        stripped = float(data[0:4])
+        voltage = stripped*(3.3/682) #convert to voltage (mV)
+        x.append(voltage)
+        y.append(i*0.1)
+    
+        plt.cla()
+        plt.plot(y, x)
+        plt.xlabel("Time [ms]")
+        plt.ylabel("Voltage [mV]")
+        plt.pause(0.1) 
+        plt.show()
+        i+=0.1
+        
+        sample = x[-200:]
+        if(len(sample) == 200):
+            fft_sample = signal.convolve(np.array(sample), filter, mode='same') #sample filtered
+            fig=plt.figure(2)
+            plt.ion()
+            plt.plot(t, fft_sample)
+            plt.xlabel('frequency (hz)')
+            plt.ylabel('amplitude (mV)')
+            plt.pause(0.1)
+            plt.show()
 
 
 #Ref: https://github.com/HighVoltages/Python-arduino-realtime-graph/blob/master/Python%20code/realtime.py 
