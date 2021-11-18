ed36a6ee2fdd9b6909316549c8117c6258885f7e
marioantolienzh <50441834+marioantolienzh@users.noreply.github.com>
2021-11-18 15:47:03 -0500

    working

--- communication.ino
+++ communication.ino
@@ -1,25 +1,16 @@
 
 // potentiometer.ino
-// reads a potentiometer sensor and sends the reading over serial
 
-int sensorPin = A0; // the potentiometer is connected to analog pin 0
-int ledPin = 13; // the LED is connected to digital pin 13
-int sensorValue; // an integer variable to store the potentiometer reading
-
-
+int sensorPin = A0; // the cable is connected to analog pin 0
+int sensorValue; // an integer variable to store the cable reading
+  
 void setup() { // this function runs once when the sketch starts up
-  pinMode (ledPin, OUTPUT);
   // initialize serial communication 
   Serial.begin(9600);
 }
 
-
 void loop() { // this loop runs repeatedly after setup() finishes
+
   sensorValue = analogRead(sensorPin); // read the sensor
   Serial.println(sensorValue); // output reading to the serial line
-  if (sensorValue < 500){
-    digitalWrite(ledPin , LOW );} // turn the LED off
-  else {
-    digitalWrite(ledPin , HIGH );} // keep the LED on
-  delay (100); // Pause in milliseconds before next reading
 }
